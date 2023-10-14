2c3dd2d12872a36c329b19baeeb0dba41abcc203
Peredreev.Nikita <nik.peredreev@mail.ru>
2023-10-15 01:03:48 +0400

    Task1

--- project-template/src/main/java/edu/hw1/Main.java
+++ project-template/src/main/java/edu/hw1/Main.java
@@ -2,6 +2,8 @@ package edu.hw1;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -12,16 +14,187 @@ public final class Main {
     }
 
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        LOGGER.info("Hello and welcome!");
+        //helloWorld();
+        /*System.out.println(minutesToSeconds("01:00"));
+        System.out.println(minutesToSeconds("13:56"));
+        System.out.println(minutesToSeconds("10:60"));*/
+        /*System.out.println(countDigits(4666));
+        System.out.println(countDigits(-3));
+        System.out.println(countDigits(0));*/
+        /*int[] a = new int[]
+            {
+              9,9,8
+            };
+        int[] b = new int[]
+            {
+                8,9
+            };
+        System.out.println(isNestable(a, b));*/
+        //System.out.println(fixString("hTsii  s aimex dpus rtni.g"));
+        //System.out.println(isPalindromeDescendant(363));
+        /*System.out.println(countK(1234));
+        System.out.println(rotateRight(8, 1));*/
+        //System.out.println(Main.rotateLeft(16, 1));
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 0; i <= 2; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            LOGGER.info("i = {}", i);
-        }
     }
+    public static void helloWorld()
+    {
+        LOGGER.info("Hello world!");
+    }
+    public static int minutesToSeconds(String str) {
+        String[] splitedStr = str.split(":");
+
+        int minutes = Integer.parseInt(splitedStr[0]);
+        int seconds = Integer.parseInt(splitedStr[1]);
+        if(seconds>=60 || seconds<=-1)
+        {
+            return -1;
+        }
+        return minutes*60+seconds;
+    }
+
+    public static int countDigits(int value) {
+        int count=0;
+        if(value==0)
+        {
+            return 1;
+        }
+        if(value<0)
+        {
+            value = Math.abs(value);
+        }
+        while(value>0)
+        {
+            count++;
+            value=value/10;
+        }
+        return count;
+    }
+    public static boolean isNestable(int[] a1, int[] a2) {
+        if(Arrays.stream(a1).min().getAsInt()>Arrays.stream(a2).min().getAsInt())
+        {
+            if(Arrays.stream(a2).max().getAsInt()>Arrays.stream(a1).max().getAsInt())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String fixString(String str) {
+        char[] charStr = str.toCharArray();
+
+        for (int i = 0; i < charStr.length - 1; i += 2) {
+            char tmp = charStr[i];
+            charStr[i] = charStr[i + 1];
+            charStr[i + 1] = tmp;
+        }
+
+        return new String(charStr);
+    }
+
+
+    public static boolean isPalindromeDescendant(int number) {
+        while (number >= 10) {
+            if (isPalindrome(number)) {
+                return true;
+            }
+            number = foundDescendant(number);
+        }
+
+        return false;
+    }
+
+    private static int foundDescendant(int number) {
+        String descendant = new String();
+        String str = Integer.toString(number);
+        for (int i = 0; i < str.length() - 1; i+=2) {
+            int digit1 = Character.getNumericValue(str.charAt(i));
+            int digit2 = Character.getNumericValue(str.charAt(i + 1));
+            descendant+=(digit1+digit2);
+        }
+
+        if (str.length() % 2 != 0) {
+            descendant+=(str.charAt(str.length() - 1));
+        }
+
+        return Integer.parseInt(descendant);
+    }
+
+    private static boolean isPalindrome(int number) {
+        String str = Integer.toString(number);
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+
+    public static int countK(int value) {
+        if (value == 6174) {
+            return 0;
+        }
+
+        return 1 + countK(sortDigitsInNumber(value));
+    }
+
+    private static int sortDigitsInNumber(int number) {
+        String numberString = String.valueOf(number);
+        char[] digitsArray = numberString.toCharArray();
+        Arrays.sort(digitsArray);
+        StringBuilder sortedNumberString = new StringBuilder(new String(digitsArray));
+        sortedNumberString.reverse();
+        int val1 =  Integer.parseInt(sortedNumberString.toString());
+        int val2 = Integer.parseInt(new String(digitsArray));
+        return val1-val2;
+    }
+/*
+    public static int rotateLeft(int n, int shift) {
+
+        var f= Integer.toBinaryString(n);
+        System.out.println(f);
+        return (n << shift) | (n >>> (f.length() - shift));
+    }
+
+    public static int rotateRight(int n, int shift) {
+
+        return (n >> shift) | (n << (Integer.SIZE - shift));
+
+    }*/
+    public static int rotateRight(int n, int shift) {
+        String binaryStr = Integer.toBinaryString(n);
+        shift %= binaryStr.length();
+        String rotatedBinaryStr = binaryStr.substring(binaryStr.length() - shift)
+            + binaryStr.substring(0, binaryStr.length() - shift);
+        return Integer.parseInt(rotatedBinaryStr, 2);
+    }
+
+    public static int rotateLeft(int n, int shift) {
+        String binaryStr = Integer.toBinaryString(n);
+        shift %= binaryStr.length();
+        String rotatedBinaryStr = binaryStr.substring(shift) + binaryStr.substring(0, shift);
+        return Integer.parseInt(rotatedBinaryStr, 2);
+    }
+    public static String toBinaryString(int n)
+    {
+        return String.format("%32s", Integer.toBinaryString(n))
+            .replaceAll(" ", "0");
+    }
+
+    public static boolean knightBoardCapture(int[][] board) {
+         int[] dx = {-2, -2, -1, -1, 1, 1, 2, 2};
+         int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == 1) {
+                    for (int k = 0; k < 8; k++) {
+                        int x = i + dx[k];
+                        int y = j + dy[k];
+                        if (x >= 0 && x < 8 && y >= 0 && y < 8 && board[x][y] == 1) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+
 }
--- project-template/src/test/java/edu/hw1/SampleTest.java
+++ project-template/src/test/java/edu/hw1/SampleTest.java
@@ -6,17 +6,141 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SampleTest {
     @Test
-    @DisplayName("Фильтрация четных чисел")
-    void filterEvenNumbers() {
-        // given
-        int[] numbers = new int[] {1, 2, 3, 4, 5};
+    @DisplayName("Тест определения секунд")
 
-        // when
-        int[] evenNumbers = EvenArrayUtils.filter(numbers);
+    public void testMinutesToSeconds() {
+        String[] testCases = new String[] {"01:00", "13:56", "10:60", "10:-1"};
+        int[] expectedResults = new int[] {60, 836, -1, -1};
 
-        // then
-        assertThat(evenNumbers)
-            .containsExactly(2, 4)
-            .hasSize(2);
+        for (int i = 0; i < testCases.length; i++) {
+            var a = Main.minutesToSeconds(testCases[i]);
+            var b = expectedResults[i];
+            assertThat(b)
+                .isEqualTo(a);
+        }
+    }
+    @Test
+    @DisplayName("Тест определения цифр в числе")
+    public void testCountDigits() {
+        int[] testCases = new int[] {4666, 566, 0, -128, -1,1};
+        int[] expectedResults = new int[] {4, 3, 1, 3, 1,1};
+        for (int i = 0; i < testCases.length; i++)
+        {
+            var a = Main.countDigits(testCases[i]);
+            var b = expectedResults[i];
+            assertThat(b)
+                .isEqualTo(a);
+        }
+    }
+    @Test
+    @DisplayName("Тест на вхождение массивов")
+
+    public void testIsNestable() {
+
+        assertThat(Main.isNestable(new int[]{1, 2, 3, 4}, new int[]{0, 6}))
+            .isEqualTo(true);
+        assertThat(Main.isNestable(new int[] {3, 1}, new int[]{4, 0}))
+            .isEqualTo(true);
+        assertThat(Main.isNestable(new int[] {9, 9, 8}, new int[]{8, 9}))
+            .isEqualTo(false);
+        assertThat(Main.isNestable(new int[] {1, 2, 3, 4}, new int[]{2, 3}))
+            .isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("Тест на нормализацию строк")
+
+    public void testFixString() {
+        String[] testCases = new String[]{"123456", "hTsii  s aimex dpus rtni.g", "badce"};
+        String[] expectedResults = new String[]{"214365", "This is a mixed up string.", "abcde"};
+
+        for (int i = 0; i < testCases.length; i++) {
+            var a = Main.fixString(testCases[i]);
+            var b = expectedResults[i];
+            assertThat(b)
+                .isEqualTo(a);
+        }
+    }
+    @Test
+    @DisplayName("Тест нахождения шагов для постоянной Капрекара")
+
+    public void testCountK() {
+        int[] testCases = new int[] {3524, 6621, 6554, 1234};
+        int[] expectedResults = new int[] {3, 5, 4, 3};
+
+        for (int i = 0; i < testCases.length; i++) {
+            var a = Main.countK(testCases[i]);
+            var b = expectedResults[i];
+            assertThat(b)
+                .isEqualTo(a);
+        }
+    }
+    @Test
+    @DisplayName("Тест на нахождение палиндрома у числа")
+
+    public void testIsPalindromeDescendant() {
+        int[] testCases = new int[] {11211230, 13001120, 23336014, 11};
+        boolean[] expectedResults =
+            new boolean[] {true, true, true, true};
+        for (int i = 0; i < testCases.length; i++) {
+            var a = Main.isPalindromeDescendant(testCases[i]);
+            var b = expectedResults[i];
+            assertThat(b)
+                .isEqualTo(a);
+        }
+    }
+    @Test
+    @DisplayName("Тест на сдвиг")
+
+    public void testRotate() {
+
+        assertThat(Main.rotateRight(8, 1))
+                .isEqualTo(4);
+        assertThat(Main.rotateLeft(16, 1))
+            .isEqualTo(1);
+        assertThat(Main.rotateLeft(17, 2))
+            .isEqualTo(6);
+
+    }
+    @Test
+    @DisplayName("Тест на коней и шахмат")
+
+    public void testKnightBoardCapture() {
+        int[][] board1 = {
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}
+        };
+        int[][] board2 = {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+        int[][] board3 = {
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+        assertThat(Main.knightBoardCapture(board1))
+            .isEqualTo(true);
+        assertThat(Main.knightBoardCapture(board2))
+            .isEqualTo(false);
+        assertThat(Main.knightBoardCapture(board3))
+            .isEqualTo(false);
     }
 }
